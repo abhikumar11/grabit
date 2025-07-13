2daff0f866d4e4c13d369f3458e3264588996647
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-07-13 09:44:23 +0530

    initial commit

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -2,13 +2,43 @@ import "./styles/category.css";
 const Category = () => {
   return (
     <div className="cat-container">
-        <div style={{display:"flex"}}>
-            <img src="https://shorturl.at/z710E" alt=""/>
-            <img src="https://shorturl.at/BrZkf" alt=""/>
-            <img src="https://shorturl.at/tHUjp" alt=""/>
-        </div>
+      <div className="img-container">
+        <a href="#">
+          <img src={"../assets/images/Slice-2_10.avif"} alt="" />
+        </a>
+        <a href="#">
+          <img src="https://shorturl.at/BrZkf" alt="" />
+        </a>
+        <a>
+          <img src="https://shorturl.at/tHUjp" alt="" />
+        </a>
+        <a href="#">
+          <img src="https://shorturl.at/z710E" alt="" />
+        </a>
+        <a href="#">
+          <img src="https://shorturl.at/BrZkf" alt="" />
+        </a>
+        <a>
+          <img src="https://shorturl.at/tHUjp" alt="" />
+        </a>
+        <a href="#">
+          <img src="https://shorturl.at/z710E" alt="" />
+        </a>
+        <a href="#">
+          <img src="https://shorturl.at/BrZkf" alt="" />
+        </a>
+        <a>
+          <img src="https://shorturl.at/tHUjp" alt="" />
+        </a>
+        <a href="#">
+          <img src="https://shorturl.at/z710E" alt="" />
+        </a>
+        <a href="#">
+          <img src="https://shorturl.at/BrZkf" alt="" />
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
