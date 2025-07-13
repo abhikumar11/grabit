6b8224058721ffb592e165353998eea6fbcc5113
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-07-13 13:07:52 +0530

    initial commit

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -4,37 +4,37 @@ const Category = () => {
     <div className="cat-container">
       <div className="img-container">
         <a href="#">
-          <img src={"../assets/images/Slice-2_10.avif"} alt="" />
+          <img src="https://shorturl.at/WtyFe" alt="" />
         </a>
         <a href="#">
-          <img src="https://shorturl.at/BrZkf" alt="" />
+          <img src="https://shorturl.at/nxrq4" alt="" />
         </a>
         <a>
-          <img src="https://shorturl.at/tHUjp" alt="" />
+          <img src="https://shorturl.at/fLvho" alt="" />
         </a>
         <a href="#">
-          <img src="https://shorturl.at/z710E" alt="" />
+          <img src="https://shorturl.at/z9uH2" alt="" />
         </a>
         <a href="#">
-          <img src="https://shorturl.at/BrZkf" alt="" />
+          <img src="https://tinyurl.com/mrw8r9eh" alt="" />
         </a>
         <a>
-          <img src="https://shorturl.at/tHUjp" alt="" />
+          <img src="https://tinyurl.com/5n8be3mc" alt="" />
         </a>
         <a href="#">
-          <img src="https://shorturl.at/z710E" alt="" />
+          <img src="https://tinyurl.com/3bp29srx" alt="" />
         </a>
         <a href="#">
-          <img src="https://shorturl.at/BrZkf" alt="" />
+          <img src="https://tinyurl.com/3syzzmpy" alt="" />
         </a>
         <a>
-          <img src="https://shorturl.at/tHUjp" alt="" />
+          <img src="https://tinyurl.com/4w8eswe8" alt="" />
         </a>
         <a href="#">
-          <img src="https://shorturl.at/z710E" alt="" />
+          <img src="https://tinyurl.com/2vc9vaxm" alt="" />
         </a>
         <a href="#">
-          <img src="https://shorturl.at/BrZkf" alt="" />
+          <img src="https://tinyurl.com/3m8fr7fp" alt="" />
         </a>
       </div>
     </div>
